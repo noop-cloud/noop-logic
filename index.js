2f8797eb01c6a31a0c104d5169b3d3ca6e66f87c
Chloe Wintzer <chloe.wintzer@rearc.io>
2020-09-03 12:11:30 -0700

    add useragent, qs, and prop

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const jsonlogic = require('json-logic-js')
 const iptool = require('node-cidr')
+const useragent = require('useragent')
+const urlparse = require('url-parse')
+const qs = require('qs')
+const dot = require('dot-object')
 
 class NoopLogic {
   constructor () {
     jsonlogic.add_operation('cidr', (cidr, ip) => {
       return iptool.cidr.includes(cidr, ip)
+    })
+    jsonlogic.add_operation('useragent', (string) => {
+      return useragent.parse(string)
+    })
+    jsonlogic.add_operation('qs', (string) => {
+      const url = urlparse(string, null, (str) => {
+        if (str.startsWith('?')) str = str.substr(1)
+        return qs.parse(str)
+      })
+      if (!url || !url.query) return {}
+      return url.query
+    })
+    jsonlogic.add_operation('prop', (prop, obj) => {
+      if (!prop) return obj
+      return dot.pick(prop, obj)
     })
   }
 
